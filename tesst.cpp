65d8fc76ae187d241e2a49dc21231efb1c6ef1eb
Arry <realarshdeepsharma@gmail.com>
2022-12-02 07:16:08 -0400

    Initial commit

---
+++ tesst.cpp
@@ -0,0 +1,78 @@
+#include<iostream>
+using namespace std;
+
+// int main(){
+//     cout << "Hello India" <<endl;
+// }
+
+// int main(){
+    
+//     int a = 123;
+
+//     cout<< a <<endl;
+// }
+
+// int main(){
+    
+//     float a = 10.2;
+
+//     cout<< a <<endl;
+// }
+
+// int main(){
+    
+//     double a = 10.22;
+
+//     cout<< a <<endl;
+// }
+
+
+// int main(){
+    
+//     char b = 'v';
+
+//     cout<< b <<endl;
+// }
+
+// int main(){
+
+//     bool bl = true;
+//     bool fbl = false;
+
+//     cout<< bl << endl;
+//     cout<< fbl << endl;
+
+// }
+
+int main(){  
+    
+    
+//     int a = 123;
+//     cout<< a <<endl;
+
+//     float f = 123.1;
+//     cout<< f << endl;
+
+//     double d = 123.12;
+//     cout<< d << endl;
+
+//     char c = 'H';
+//     cout<< c << endl;
+
+//     bool tb = true;
+//     bool fb = false;
+//     cout<< tb << endl;
+//     cout<< fb << endl;
+
+//     int size = sizeof(c);
+//     cout<< "Size of a is: "<< size <<endl;
+// 
+    int trial = 'a';
+    cout<< trial << endl;
+
+    char lol = 98;
+    cout<< lol << endl;
+}
+
+
+
