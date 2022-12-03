df5ede04b193128fd5f7c7ec41766bb2bbc95e3a
Arry <realarshdeepsharma@gmail.com>
2022-12-03 05:15:32 -0400

    Conditional and loops practice file added

---
+++ ConditionalsAndLoop.cpp
@@ -0,0 +1,26 @@
+ #include <iostream>
+ using namespace std;
+
+ int main(){
+    // This is what I coded blindly
+    // int a = 55;
+    // int b = 8;
+    // if (a>b){
+    //     cout << "A is big" << endl;
+    // }
+    // else{
+    //     cout << "B is big" << endl;
+    // }
+
+
+/*
+Important to remember!
+cout is for displaying output
+cin is for taking in put
+code will not move forward if not input is recieved
+c>>n;
+*/
+    int n;
+    cin>>n;
+    // cout<< "Value of n is: " << n << endl; 
+ }
