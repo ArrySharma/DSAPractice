b7354ea8c99ea616519240df4e1cb412a5b76111
Arry <realarshdeepsharma@gmail.com>
2022-12-04 04:50:18 -0400

    IncNumTriaPattern

---
+++ increasingNumTriaPattern.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>> n;
+    int i = 1;
+    int counter = 1;
+
+    while(i<=n){
+        int j = 1;
+        while(j<=i){
+            cout<< " " << counter;
+            counter++;
+            j++;
+        }
+        cout<< endl;
+        i++;
+
+
+
+
+
+    }
+
+
+
+
+
+}
